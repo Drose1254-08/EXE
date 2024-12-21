181371a079660dedc838d95e7df20e90c762c58e
Drose1254-08 <128402386+Drose1254-08@users.noreply.github.com>
2024-12-21 18:09:51 +0800

    Update Fun.java

--- untitled/src/Fun.java
+++ untitled/src/Fun.java
@@ -3,7 +3,7 @@
  * @date 2024/12/21  17:28
  */
 public class Fun {
-    public static String foo(String a,String b,String c){
+    public static String foo(String a,String b,String c，String d){
         return a+b+c;
     }
 }
