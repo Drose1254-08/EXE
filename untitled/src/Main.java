b0304e38b31475193c7d11ca8049996b1923f192
is__wind <772265647@qq.com>
2024-12-21 17:48:13 +0800

    aaaa

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,6 +5,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(Fun.foo("a", "b"));
+        System.out.println(Fun.foo("a", "b","c"));
     }
 }
---
+++ untitled/src/aaaaa.java
@@ -0,0 +1,6 @@
+/**
+ * @author Drose
+ * @date 2024/12/21  17:47
+ */
+public class aaaaa {
+}
